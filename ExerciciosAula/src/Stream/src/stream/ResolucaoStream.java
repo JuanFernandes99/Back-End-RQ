1c38b171de92476d438401afe467d89b92fdf0b0
JuanFernandes99 <juandanieldosr@gmail.com>
2022-02-08 18:02:39 +0000

    a

---
+++ ExerciciosAula/src/Stream/src/stream/ResolucaoStream.java
@@ -0,0 +1,20 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ResolucaoStream {
+
+	public static void main(String args[]) {
+		List<String> myList =
+				Arrays.asList("a1","a2","b1","b2","c2","c1");
+		myList
+		
+		.stream()
+		.filter(s -> s.startsWith("c"))
+		.map(String::toUpperCase)
+		.sorted()
+		.forEach(System.out::println);
+		
+	}
+}
